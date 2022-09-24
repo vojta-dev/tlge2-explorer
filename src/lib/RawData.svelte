be4b2d07b8bc9975a2aac1b47a490bc0a80322db
Vojta <65811166+vojta-dev@users.noreply.github.com>
2022-09-24 12:41:49 +0200

    Improve raw data

--- src/lib/RawData.svelte
+++ src/lib/RawData.svelte
@@ -1,13 +1,27 @@
 <script>
   export let level;
+
+  const KEYS = [
+    ['TYPE_OF_LEVEL_EASY', 'ARGS_EASY', 'ENGLISH_EASY'],
+    ['TYPE_OF_LEVEL', 'ARGS', 'ENGLISH'],
+  ];
 </script>
 
 <table>
-  {#each Object.keys(level) as key}
-    <tr>
-      <td>{key}</td>
-      <td>{level[key]}</td>
-    </tr>
+  {#each KEYS as table, i}
+    {#each table as key, j}
+      <tr>
+        {#if j === 0}
+          <td rowspan="3" class="nowrap">{i === 0 ? 'NORMAL' : 'HARD'}</td>
+        {/if}
+        <td class="nowrap">{key.replaceAll('_', ' ').replace('EASY', '')}</td>
+        {#if level[key] !== ''}
+          <td>{level[key]}</td>
+        {:else}
+          <td>-</td>
+        {/if}
+      </tr>
+    {/each}
   {/each}
 </table>
 
@@ -18,5 +32,11 @@
     border-collapse: collapse;
     padding: 1rem;
     margin: auto;
+    text-align: initial;
+  }
+
+  .nowrap {
+    white-space: nowrap;
+    text-align: center;
   }
 </style>
